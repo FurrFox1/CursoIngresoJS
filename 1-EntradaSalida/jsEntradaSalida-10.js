4f9244d46b4915245080f3eaa5d612e432f6063c
FurrFox1 <gonzarojas65@gmail.com>
2023-02-03 09:54:03 -0300

    Ejercicios Resueltos 10 E/S

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let sueldo;
+	let descuento;
+	let sueldoDescontado;
+
+	sueldo = parseFloat(document.getElementById("txtIdImporte").value);
+
+	descuento = sueldo * 25 /100;
+	sueldoDescontado = sueldo - descuento;
+	
+	document.getElementById("txtIdResultado").value = sueldoDescontado;
 }
+
+//Copyright(?) Gonzalo Rojas Grupo 3 Div D
